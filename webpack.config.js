515fa0abc8b20fb91d2dc1a3c2a514c7ec5d306c
jaketrent <trent.jake@gmail.com>
2015-06-12 13:22:46 -0600

    css webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: [
+    './index.css'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.css'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'css' }
+    ]
+  }
+}
